a9c84a2e5c314fc6e82c88484095e6730f028606
Nathan L Smith <nlloyds@gmail.com>
2013-03-07 00:00:19 -0600

    initial commit

---
+++ 404.php
@@ -0,0 +1,4 @@
+<?php get_header(); ?>
+    <h2>Page Not Found</h2>
+    <p>Sorry, we couldn't find that page.</p>
+<?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,16 @@
+        <div id="menu">
+          <div class="menu-section first">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/gallery/">Gallery</a></li>
+              <li><a href="/documents/">Documents</a></li>
+              <li><a href="/links/">Links</a></li>
+              <li><a href="/calendar/">Calendar</a></li>
+              <li><a href="/auction/">Auction</a></li>
+              <li><a href="/things-to-do">Things to Do</a></li>
+            </ul>
+          </div>
+          <div class="menu-section last">
+        <!-- <span class="countdown"><span class="days">-548</span> <span class="message">days until the 2009 Gottung Reunion</span></span> -->
+          </div>
+        </div>
